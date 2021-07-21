b3e9e891620a366d419dc2762cc9231c7151c565
Disha <disha4091@gmail.com>
2021-07-21 09:00:09 +0530

    Connected mySQL

---
+++ server/config.js
@@ -0,0 +1,7 @@
+const Config_details = { 
+    host: "localhost",
+    user: "root",
+    password: "mySQL@2020",
+    database: "hostel_management_system",
+}
+module.exports = Config_details ;
---
+++ server/index.js
@@ -0,0 +1,49 @@
+import Config_details from './config'
+const express = require('express') ;
+const app = express() ;
+const mysql = require('mysql') ;
+const cors = require('cors') ; 
+
+app.use(cors()) ;
+app.use(express.json()) ;
+
+
+const db = {
+    host: Config_details.host,
+    user: Config_details.root,
+    password: Config_details.password,
+    database:Config_details.database,
+    port: 3306,
+    insecureAuth: true
+};
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true
+}));
+
+const con = mysql.createConnection(db);
+
+app.post('/create' , (req,res)=>{
+    const UserId = req.body.UserId
+    const Username = req.body.Username
+    const Email = req.body.Email
+    const Area_pin = req.body.Area_pin
+    const Area = req.body.Area
+    const City = req.body.City
+
+    con.query("INSERT INTO `users` (UserId, Username, Email,Area_pin, Area, City) VALUES(?,?,?,?,?,?)"
+    , [UserId, Username, Email,Area_pin, Area, City] , (err, result) => {
+        if(err) {
+            console.log(err);
+        }
+        else{
+            res.send("Values Inserted") ;
+        }
+    }) ;
+
+})
+
+app.listen(3001, ()=>{
+    console.log("Server running");
+})
